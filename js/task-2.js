e67eee69851c4e98cb4b649b64c4c5c2a7b6e4a0
Andrii Kondysiuk <kondysiuka@gmail.com>
2020-11-23 16:49:54 +0000

    task-2

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+const total = 100;
+const order = 130;
+
+if (order > total) {
+  console.log('На складе недостаточно твоаров!');
+}
+
+if (order <= total) {
+  console.log('Заказ оформлен, с вами свяжется менеджер');
+}
