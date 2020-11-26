b210d9763dd3b3d8fbb68d447d6814412eaa79c9
Andrii Kondysiuk <kondysiuka@gmail.com>
2020-11-26 17:19:52 +0000

    update task-4

--- js/task-4.js
+++ js/task-4.js
@@ -3,33 +3,14 @@ const pricePerDroid = 3000;
 
 const number = prompt('Сколько ты хочешь купить дроидов');
 
-if (number === null) 
+if (number === null) {
 console.log('Отменено пользователем!');
+}
 
 const totalPrice = number * pricePerDroid;
+if (totalPrice <= credits && number !==null ){
+console.log(`Вы купили ${number} дроидов, на счету осталось ${credits - totalPrice} кредитов.`);
 
-if (totalPrice > credits) {
+}if (credits < totalPrice){
   console.log('Недостаточно средств на счету!');
-} 
-else
-  console.log(
-    `Вы купили ${number} дроидов, на счету осталось 
-      ${credits - totalPrice}
-     кредитов.`,
-  );
-
-// const credits = 23580;
-// const pricePerDroid = 3000;
-// const number = prompt('Сколько ты хочешь купить дроидов');
-// const totalPrice = number * pricePerDroid;
-// const restCredits = credits - number * pricePerDroid;
-
-// console.log(totalPrice)
-// while (number === null) {
-//     console.log('Отменено пользователем!');
-//     break;
-//  } if (totalPrice <= credits){
-// console.log(`Вы купили ${number} дроидов,на счету осталось ${restCredits} кредитов`);
-//  } else   {
-//     console.log('Недостаточно средств на счету!');
-//  }
+}
